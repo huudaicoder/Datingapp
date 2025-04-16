215fbff72951b7ecc8fa245f8c62c16700b78793
huudaicoder <dai.dh2k1@gmail.com>
2025-04-16 17:03:05 +0700

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,15 @@
-using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController(DataContext context) : ControllerBase
+[Authorize]
+public class UsersController(DataContext context) : BaseApiController
 {
-  
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -18,6 +17,7 @@ public class UsersController(DataContext context) : ControllerBase
         return users;
     }
 
+    [Authorize]
     [HttpGet("{id:int}")] // /api/users/3   (id)
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
@@ -25,4 +25,15 @@ public class UsersController(DataContext context) : ControllerBase
         if (user == null) return NotFound();
         return user;
     }
+    
+    // public async Task<IActionResult> DeleteUser(int id)
+    // {
+    //     var user = await context.Users.FindAsync(id);
+    //     if (user == null) return NotFound();
+
+    //     context.Users.Remove(user);
+    //     await context.SaveChangesAsync();
+    //     return Ok($"User với ID {id} đã bị xóa.");
+    // }
+    
 }
